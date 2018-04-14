3b01b11412904472aed1ed3cd4c936f5ac8fa14f
Emiliano Legna <emi_legna@yahoo.com.ar>
2018-04-14 15:47:39 -0300

    Commiteado solo para verlo con Esteban. Bugfix de un error al traer datos de algunos servicios. Se comentan ruteos que no se estarían usando

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,10 @@ use App\Models\Usuario;
 |
 */
 
-Route::group(['middleware' => 'cors'], function () {
-    Route::post('/login', 'Auth\LoginController@login');
-    Route::post('/usuarioLogueado', 'Auth\LoginController@getAuthenticatedUser');
-});
+// Route::group(['middleware' => 'cors'], function () {
+    // Route::post('/login', 'Auth\LoginController@login');
+    // Route::post('/usuarioLogueado', 'Auth\LoginController@getAuthenticatedUser');
+// });
 
 
 /*
@@ -25,8 +25,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
 
-Route::resource('usuarios', 'UsuarioController');
-Route::resource('alumnos', 'AlumnoController');
+// Route::resource('usuarios', 'UsuarioController');
+// Route::resource('alumnos', 'AlumnoController');
 
 /*
 Route::get('users', function () {
