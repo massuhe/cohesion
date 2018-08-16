19f85b299fd082c62518930303e511bd193d4ee3
Emiliano Legna <emi_legna@yahoo.com.ar>
2018-08-15 21:18:05 -0300

    #18 Ver listado de pagos - Unit Tests: Tests para el método 'getByAlumnoYFechas' de PagoService. Se completan los tests de unidad para el PagoService. Se agrega --testdox para visualizar listado al correr 'composer tests'

--- tests/Unit/Finanzas/PagoServiceTest.php
+++ tests/Unit/Finanzas/PagoServiceTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Business\Finanzas\Models\Pago;
 use Business\Finanzas\Services\PagoService;
 use Business\Finanzas\Repositories\PagoRepository;
+use Carbon\Carbon;
 
 class PagoServiceTest extends TestCase
 {
@@ -63,6 +64,77 @@ class PagoServiceTest extends TestCase
 
         $pagoService = new PagoService($pagoRepositoryMock);
         $returned = $pagoService->getByMesCuota(2, 4);
+
+        $this->assertTrue(is_array($returned));
+        $this->assertEquals(2, count($returned));
+        $this->assertEquals(1111, $returned[0]->identificadorFicticio);
+        $this->assertEquals(2222, $returned[1]->identificadorFicticio);
+    }
+
+    public function testGetByAlumnoYFechas_RetornaVacio_DelRepository()
+    {
+        $pagoRepositoryMock = $this->getMockBuilder(PagoRepository::class)
+                                ->setMethods(['getByAlumnoYFechas'])
+                                ->getMock();
+
+        $idAlumno = 5;
+        $fechaDesde = Carbon::create(2018, 1, 1, 0, 0, 0);
+        $fechaHasta = Carbon::create(2018, 7, 1, 0, 0, 0);
+        $empty = array();
+        $pagoRepositoryMock->expects($this->once())
+                            ->method('getByAlumnoYFechas')
+                            ->with($this->equalTo($idAlumno), $this->equalTo($fechaDesde), $this->equalTo($fechaHasta))
+                            ->willReturn($empty);
+        
+        $pagoService = new PagoService($pagoRepositoryMock);
+        $returned = $pagoService->getByAlumnoYFechas($idAlumno, $fechaDesde, $fechaHasta);
+
+        $this->assertTrue(is_array($returned));
+        $this->assertEquals(0, count($returned));
+    }
+
+    public function testGetByAlumnoYFechas_RetornaNull_DelRepository()
+    {
+        $pagoRepositoryMock = $this->getMockBuilder(PagoRepository::class)
+                                ->setMethods(['getByAlumnoYFechas'])
+                                ->getMock();
+        
+        $idAlumno = 5;
+        $fechaDesde = Carbon::create(2018, 1, 1, 0, 0, 0);
+        $fechaHasta = Carbon::create(2018, 7, 1, 0, 0, 0);
+        $empty = array();
+        $pagoRepositoryMock->expects($this->once())
+                            ->method('getByAlumnoYFechas')
+                            ->with($this->equalTo($idAlumno), $this->equalTo($fechaDesde), $this->equalTo($fechaHasta))
+                            ->willReturn(null);
+        
+        $pagoService = new PagoService($pagoRepositoryMock);
+        $returned = $pagoService->getByAlumnoYFechas($idAlumno, $fechaDesde, $fechaHasta);
+
+        $this->assertNull($returned);
+    }
+
+    public function testGetByAlumnoYFechas_RetornaPagos_DelRepository()
+    {
+        $pagoRepositoryMock = $this->getMockBuilder(PagoRepository::class)
+                                ->setMethods(['getByAlumnoYFechas'])
+                                ->getMock();
+        $pago1 = new Pago();
+        $pago1->identificadorFicticio = 1111;
+        $pago2 = new Pago();
+        $pago2->identificadorFicticio = 2222;
+        $arrayPagos = array($pago1, $pago2);
+        $idAlumno = 5;
+        $fechaDesde = Carbon::create(2018, 1, 1, 0, 0, 0);
+        $fechaHasta = Carbon::create(2018, 7, 1, 0, 0, 0);
+        $empty = array();
+        $pagoRepositoryMock->expects($this->once())
+                            ->method('getByAlumnoYFechas')
+                            ->with($this->equalTo($idAlumno), $this->equalTo($fechaDesde), $this->equalTo($fechaHasta))
+                            ->willReturn($arrayPagos);
+
+        $pagoService = new PagoService($pagoRepositoryMock);
+        $returned = $pagoService->getByAlumnoYFechas($idAlumno, $fechaDesde, $fechaHasta);
 
         $this->assertTrue(is_array($returned));
         $this->assertEquals(2, count($returned));
