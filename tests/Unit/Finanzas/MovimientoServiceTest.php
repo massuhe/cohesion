31d7e3219b20e5802d8f340e8e572b80ddb47a87
Emiliano Legna <emi_legna@yahoo.com.ar>
2018-08-27 00:47:49 -0300

    #25 Gestionar Movimientos fijos - Unit Tests: Tests para el método 'storeOrUpdate' de MovimientoService. Ejemplo de mock validando varias llamadas (MovimientoFactory) con diferentes parámetros y que devuelva diferentes cosas en consecutivas llamadas (testStoreOrUpdate_ConMovimientos_BorraDelMes_GuardaYRetornaMovimientos).

--- tests/Unit/Finanzas/MovimientoServiceTest.php
+++ tests/Unit/Finanzas/MovimientoServiceTest.php
@@ -146,4 +146,213 @@ class MovimientoServiceTest extends TestCase
         $movimientoService->findOrReturnLatest($mes, $anio);
     }
 
+    public function testStoreOrUpdate_SinMovimientosdelMes_BorraYGuardaVacio_RetornaVacioDelRepo()
+    {
+        $movimientoRepositoryMock = $this->getMockBuilder(MovimientoRepository::class)
+                                ->setMethods(['deleteByMesAnio', 'storeMany', 'getWhereArray'])
+                                ->getMock();
+        $movimientoFactoryMock = $this->getMockBuilder(MovimientoFactory::class)
+                                ->setMethods(['createMovimiento'])
+                                ->getMock();
+        
+        $movimientoFactoryMock->expects($this->never())
+                            ->method('createMovimiento');
+        $mes = 5;   // Cualquier número de mes
+        $anio = 2018;   // Cualquier número de año
+        $mesYAnioArray = [
+            ['mes', $mes],
+            ['anio', $anio]
+        ];
+        $empty = collect([]);
+        $anterioresVacio = collect([]);
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('deleteByMesAnio')
+                            ->with($this->equalTo($mes), $this->equalTo($anio));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('storeMany')
+                            ->with($this->equalTo($empty));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('getWhereArray')
+                            ->with($this->equalTo($mesYAnioArray))
+                            ->willReturn($anterioresVacio);
+                            
+        $nuevosMovimientosVacio = array();
+        $movimientoService = new MovimientoService($movimientoRepositoryMock, $movimientoFactoryMock);
+        $returned = $movimientoService->storeOrUpdate($mes, $anio, $nuevosMovimientosVacio);
+
+        $this->assertInstanceOf('Illuminate\Support\Collection', $returned);
+        $this->assertEquals(0, $returned->count());
+    }
+
+    public function testStoreOrUpdate_SinMovimientosdelMes_BorraYGuardaVacio_RetornaMovimientosDelRepo()
+    {
+        $movimientoRepositoryMock = $this->getMockBuilder(MovimientoRepository::class)
+                                ->setMethods(['deleteByMesAnio', 'storeMany', 'getWhereArray'])
+                                ->getMock();
+        $movimientoFactoryMock = $this->getMockBuilder(MovimientoFactory::class)
+                                ->setMethods(['createMovimiento'])
+                                ->getMock();
+        
+        $movimientoFactoryMock->expects($this->never())
+                            ->method('createMovimiento');
+        $mes = 5;   // Cualquier número de mes
+        $anio = 2018;   // Cualquier número de año
+        $mesYAnioArray = [
+            ['mes', $mes],
+            ['anio', $anio]
+        ];
+        $empty = collect([]);
+        $movimiento1 = new Movimiento();
+        $movimiento1->identificadorFicticio = 1111;
+        $movimiento2 = new Movimiento();
+        $movimiento2->identificadorFicticio = 2222;
+        $arrayMovimientos = collect([$movimiento1, $movimiento2]);
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('deleteByMesAnio')
+                            ->with($this->equalTo($mes), $this->equalTo($anio));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('storeMany')
+                            ->with($this->equalTo($empty));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('getWhereArray')
+                            ->with($this->equalTo($mesYAnioArray))
+                            ->willReturn($arrayMovimientos);
+                            
+        $nuevosMovimientosVacio = array();
+        $movimientoService = new MovimientoService($movimientoRepositoryMock, $movimientoFactoryMock);
+        $returned = $movimientoService->storeOrUpdate($mes, $anio, $nuevosMovimientosVacio);
+
+        $this->assertInstanceOf('Illuminate\Support\Collection', $returned);
+        $this->assertEquals(2, $returned->count());
+        $movi1 = $returned[0];
+        $this->assertInstanceOf('Business\Finanzas\Models\Movimiento', $movi1);
+        $this->assertEquals(1111, $movi1->identificadorFicticio);
+        $movi2 = $returned[1];
+        $this->assertInstanceOf('Business\Finanzas\Models\Movimiento', $movi2);
+        $this->assertEquals(2222, $movi2->identificadorFicticio);
+    }
+
+    public function testStoreOrUpdate_ConUnMovimiento_BorraDelMes_GuardaYRetornaElMovimiento()
+    {
+        $movimientoRepositoryMock = $this->getMockBuilder(MovimientoRepository::class)
+                                ->setMethods(['deleteByMesAnio', 'storeMany', 'getWhereArray'])
+                                ->getMock();
+        $movimientoFactoryMock = $this->getMockBuilder(MovimientoFactory::class)
+                                ->setMethods(['createMovimiento'])
+                                ->getMock();
+        
+        $mes = 5;   // Cualquier número de mes
+        $anio = 2018;   // Cualquier número de año
+        $datosMovimiento1 = [
+            'identificadorFicticio' => 1111,
+            'importe' => 400
+        ];
+        $datosMovimientoFactoryParam1 = [
+            'identificadorFicticio' => 1111,
+            'importe' => 400,
+            'mes' => $mes,
+            'anio' => $anio
+        ];
+        $movimiento1 = new Movimiento();
+        $movimiento1->identificadorFicticio = 1111;
+        $movimientoFactoryMock->expects($this->once())
+                            ->method('createMovimiento')
+                            ->with($this->equalTo($datosMovimientoFactoryParam1))
+                            ->willReturn($movimiento1);
+        $mesYAnioArray = [
+            ['mes', $mes],
+            ['anio', $anio]
+        ];
+        $arrayMovimientos = collect([$movimiento1]);
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('deleteByMesAnio')
+                            ->with($this->equalTo($mes), $this->equalTo($anio));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('storeMany')
+                            ->with($this->equalTo($arrayMovimientos));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('getWhereArray')
+                            ->with($this->equalTo($mesYAnioArray))
+                            ->willReturn($arrayMovimientos);
+                            
+        $nuevosMovimientos = array($datosMovimiento1);
+        $movimientoService = new MovimientoService($movimientoRepositoryMock, $movimientoFactoryMock);
+        $returned = $movimientoService->storeOrUpdate($mes, $anio, $nuevosMovimientos);
+
+        $this->assertInstanceOf('Illuminate\Support\Collection', $returned);
+        $this->assertEquals(1, $returned->count());
+        $movi1 = $returned[0];
+        $this->assertInstanceOf('Business\Finanzas\Models\Movimiento', $movi1);
+        $this->assertEquals(1111, $movi1->identificadorFicticio);
+    }
+
+    public function testStoreOrUpdate_ConMovimientos_BorraDelMes_GuardaYRetornaMovimientos()
+    {
+        $movimientoRepositoryMock = $this->getMockBuilder(MovimientoRepository::class)
+                                ->setMethods(['deleteByMesAnio', 'storeMany', 'getWhereArray'])
+                                ->getMock();
+        $movimientoFactoryMock = $this->getMockBuilder(MovimientoFactory::class)
+                                ->setMethods(['createMovimiento'])
+                                ->getMock();
+        
+        $mes = 5;   // Cualquier número de mes
+        $anio = 2018;   // Cualquier número de año
+        $datosMovimiento1 = [
+            'identificadorFicticio' => 1111,
+            'importe' => 400
+        ];
+        $datosMovimiento2 = [
+            'identificadorFicticio' => 2222,
+            'importe' => 500
+        ];
+        $datosMovimientoFactoryParam1 = [
+            'identificadorFicticio' => 1111,
+            'importe' => 400,
+            'mes' => $mes,
+            'anio' => $anio
+        ];
+        $datosMovimientoFactoryParam2 = [
+            'identificadorFicticio' => 2222,
+            'importe' => 500,
+            'mes' => $mes,
+            'anio' => $anio
+        ];
+        $movimiento1 = new Movimiento();
+        $movimiento1->identificadorFicticio = 1111;
+        $movimiento2 = new Movimiento();
+        $movimiento2->identificadorFicticio = 2222;
+        $movimientoFactoryMock->expects($this->exactly(2))
+                            ->method('createMovimiento')
+                            ->withConsecutive([$this->equalTo($datosMovimientoFactoryParam1)], [$this->equalTo($datosMovimientoFactoryParam2)])
+                            ->will($this->onConsecutiveCalls($movimiento1, $movimiento2));
+        $mesYAnioArray = [
+            ['mes', $mes],
+            ['anio', $anio]
+        ];
+        $arrayMovimientos = collect([$movimiento1, $movimiento2]);
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('deleteByMesAnio')
+                            ->with($this->equalTo($mes), $this->equalTo($anio));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('storeMany')
+                            ->with($this->equalTo($arrayMovimientos));
+        $movimientoRepositoryMock->expects($this->once())
+                            ->method('getWhereArray')
+                            ->with($this->equalTo($mesYAnioArray))
+                            ->willReturn($arrayMovimientos);
+                            
+        $nuevosMovimientos = array($datosMovimiento1, $datosMovimiento2);
+        $movimientoService = new MovimientoService($movimientoRepositoryMock, $movimientoFactoryMock);
+        $returned = $movimientoService->storeOrUpdate($mes, $anio, $nuevosMovimientos);
+
+        $this->assertInstanceOf('Illuminate\Support\Collection', $returned);
+        $this->assertEquals(2, $returned->count());
+        $movi1 = $returned[0];
+        $this->assertInstanceOf('Business\Finanzas\Models\Movimiento', $movi1);
+        $this->assertEquals(1111, $movi1->identificadorFicticio);
+        $movi2 = $returned[1];
+        $this->assertInstanceOf('Business\Finanzas\Models\Movimiento', $movi2);
+        $this->assertEquals(2222, $movi2->identificadorFicticio);
+    }
+
 }
